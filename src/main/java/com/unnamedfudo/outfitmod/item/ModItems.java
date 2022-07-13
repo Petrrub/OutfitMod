388c1907267871008273e3cc55f76460ff0a0d41
Petrrub <petrrub@outlook.com>
2022-07-13 18:23:05 +0300

    Fixed Registry

--- src/main/java/com/unnamedfudo/outfitmod/item/ModItems.java
+++ src/main/java/com/unnamedfudo/outfitmod/item/ModItems.java
@@ -1,9 +1,6 @@
 package com.unnamedfudo.outfitmod.item;
 
 import com.unnamedfudo.outfitmod.OutritMod;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,7 +15,7 @@ public class ModItems {
   //          () -> new ArmorItem(ModArmorMaterial.STRAW, EquipmentSlot.HEAD,new  Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> STRAW_HAT = ITEMS.register("straw_hat",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new ItemHat("straw_hat", 1));
 
 
     public static void register(IEventBus eventBus) {
